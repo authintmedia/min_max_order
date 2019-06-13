cd5f33bc46583d2f7ef69d00372d8d12507de06d
Authint Media <authintmedia@users.noreply.github.com>
2019-06-13 21:37:50 +0530

    Update ValidateMinMaxSubscriber.php

--- src/EventSubscriber/ValidateMinMaxSubscriber.php
+++ src/EventSubscriber/ValidateMinMaxSubscriber.php
@@ -1,83 +1,42 @@
 <?php
+
+namespace Drupal\minimum_maximum_order\EventSubscriber;
+
+use Drupal\commerce_order\Event\OrderItemEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\state_machine\Event\WorkflowTransitionEvent;
+
 /**
- * @file
- * Contains Drupal\minimum_maximum_order\Form\MinimumMaximumOrderForm.
+ * Class EntityTypeSubscriber.
+ *
+ * @package Drupal\custom_events\EventSubscriber
  */
-namespace Drupal\minimum_maximum_order\Form;
-use Drupal\Core\Form\ConfigFormBase;
-use Drupal\Core\Form\FormStateInterface;
+class ValidateMinMaxSubscriber implements EventSubscriberInterface {
 
-class MinimumMaximumOrderForm extends ConfigFormBase {
 
   /**
    * {@inheritdoc}
+   *
+   * @return array
+   *   The event names to listen for, and the methods that should be executed.
    */
-  protected function getEditableConfigNames() {
+  public static function getSubscribedEvents() {
     return [
-      'minimum_maximum_order.adminsettings',
+      'commerce_order.place.pre_transition' => ['validateMinMax', -10],
     ];
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getFormId() {
-    return 'minimum_maximum_order_form';
-  }
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('minimum_maximum_order.adminsettings');
+  public function validateMinMax(WorkflowTransitionEvent $event) {
+    $order = $event->getEntity();
+    $total = (int) $order->getTotalPrice()->getNumber();
+    $min = 30;
+    $max = null;
 
-    $form['minimum_order'] = [
-      '#type' => 'number',
-      '#title' => $this->t('Minimum Order'),
-      '#description' => $this->t('Minimum order amount needed to go to checkout page'),
-      '#default_value' => $config->get('minimum_order')
-    ];
+    if ( $min !== null && $total < $min) {
+      return false;
+    } elseif ( $max !== null && $total > $max) {
 
-    $form['maximum_order'] = [
-      '#type' => 'number',
-      '#title' => $this->t('Maximum Order'),
-      '#description' => $this->t('Maximum order amount needed to go to checkout page'),
-      '#default_value' => $config->get('maximum_order'),
-    ];
-
-    return parent::buildForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    $min = $form_state->getValue('minimum_order');
-    $max = $form_state->getValue('maximum_order');
-    if (!empty($min)) {
-      if ($min < 0) {
-        $form_state->setError($form['minimum_order'],'Value for minimum order must be greater than 0');
-      }
-    }
-    if (!empty($max)) {
-      if ($form_state->getValue('maximum_order') < 0) {
-        $form_state->setError($form['maximum_order'],'Value for maximum order must be greater than 0');
-      }
-    }
-    if (!empty($min) && !empty($max) && $min >= $max) {
-      $form_state->setError($form['minimum_order'],'Value for minimum order must be less than the maximum order');
     }
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-
-    parent::submitForm($form, $form_state);
-
-    $this->config('minimum_maximum_order.adminsettings')
-      ->set('minimum_order', $form_state->getValue('minimum_order'))
-      ->set('maximum_order', $form_state->getValue('maximum_order'))
-      ->save();
-  }
 }
